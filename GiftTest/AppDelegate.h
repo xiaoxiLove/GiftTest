e797a0e5e32da04e825d655487ea86b3e5d5fc60
xiaoxi <366998502@qq.com>
2016-06-02 10:52:44 +0800

    Initial Commit

---
+++ GiftTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GiftTest
+//
+//  Created by mac on 16/6/2.
+//  Copyright © 2016年 tingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GiftTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GiftTest
+//
+//  Created by mac on 16/6/2.
+//  Copyright © 2016年 tingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
